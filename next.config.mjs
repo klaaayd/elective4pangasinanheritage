f196a96819988c3db608c487f60028898fabbd82
klaaayd <klaaayd@users.noreply.github.com>
2026-09-04 20:52:15 +0800

    Clean up docs and configure Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
